c3154b4093117c02d113b83497a5e67c28ddaf0b
Phạm Tú <phamtu@192.168.3.123>
2024-07-21 21:01:46 +0700

    commit exercise week 2

--- module_2/gray_image_avengre.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-image = plt.imread('module_2/data/dog.jpeg')
-
-gray_image = np.array(np.mean(image, axis=2))
-print(gray_image[0,0])
-plt.imshow(gray_image, cmap='gray')
-plt.show()
